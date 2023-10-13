98484fedc3f445b2fb40f854dff4fa6b8150992a
Anand Trivedi <anand.trivedi@databricks.com>
2023-10-13 12:36:01 -0400

    modified to available database and server

--- test/model-test.js
+++ test/model-test.js
@@ -10,7 +10,7 @@ const Model = require('../src/model')
 const model = new Model()
 
 test('should properly fetch from the API and translate features', t => {
-  model.getData({ params: { id: 'stuart.geospatial.oproad_motorway_junctions' } }, (err, geojson) => {
+  model.getData({ params: { id: 'geoserverat.structures_national_gdb' } }, (err, geojson) => {
     t.error(err)
     t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
     t.ok(geojson.features, 'has features')
@@ -18,9 +18,9 @@ test('should properly fetch from the API and translate features', t => {
     const feature = geojson.features[0]
     t.equal(feature.type, 'Feature', 'has proper type')
     t.equal(feature.geometry.type, 'Point', 'returns point geometries')
-    t.deepEqual(feature.geometry.coordinates, [-2.6492791853702613, 54.50751433669944], 'translates geometry correctly')
+    t.deepEqual(feature.geometry.coordinates, [-116.30138297783401, 33.67238838002538], 'translates geometry correctly')
     t.ok(feature.properties, 'creates attributes')
-    t.equal(feature.properties.junction_number, 'M6 J39', 'extracts junction_number property field correctly')
+    t.equal(feature.properties.zipcode, '92253', 'extracts zip code property field correctly')
     t.end()
   })
 })
